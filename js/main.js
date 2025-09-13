fcd916056b8ee9995094528d6d5bcffb424742f4
Adriana Castillo <adricasra1993@gmail.com>
2025-09-13 10:33:40 -0500

    Ajuste el diseño responsive para versión movil de la pagina

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
+
+    if (window.innerWidth <= 767) {
+        $('.slider-products').slick({
+            dots: false,
+            infinite: true,
+            arrows: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+        });
+    }
     
-    $('.slider-products').slick({
+    if (window.innerWidth > 767){
+        $('.slider-products').slick({
         dots: false,
         infinite: true,
         arrows: true,
@@ -33,42 +46,43 @@ $(document).ready(function () {
                 }
             }
         ]
-    });
+        });
 
-    $('.slider-reviews').slick({
-        dots: false,
-        infinite: true,
-        arrows: true,
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: false
+        $('.slider-reviews').slick({
+            dots: false,
+            infinite: true,
+            arrows: true,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: false
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
                 }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
+            ]
+        });
+    }
 
 
 
